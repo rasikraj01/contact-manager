736ca7108e4fd4794c5453ca09f4939fd2d41097
Rasik <rasikraj01@gmail.com>
2016-10-26 02:46:24 +0530

    Add files via upload

--- website/manager/urls.py
+++ website/manager/urls.py
@@ -1,13 +1,24 @@
 from django.conf.urls import include, url
-from .views import home, contact, contactList
+from .views import register,home, contact, contactList, login, logout
 
 app_name = 'manager'
 
 urlpatterns = [
     url(r'^$', home.as_view(), name='home'),
-    url(r'^contact/$', contact, name='contact'),
 
+    #CRUD
+
+    url(r'^contact/$', contact, name='contact'),
+    #url(r'^/detail/$', contact, name='detail'),
+    #url(r'^/update/$',, name='update'),
+    #url(r'^delete/$', , name='delete'),
     url(r'^list/$', contactList, name='list'),
     # url(r'^list/$', contactList.as_view(), name='list'),
+    
+
+    #REGISTRATION
+    url(r'^register/$', register.as_view() , name='register'),
+    url(r'^login/$', login.as_view() , name='login'),
+    url(r'^logout/$', logout , name='logout'),
     
     ]
--- website/manager/views.py
+++ website/manager/views.py
@@ -11,8 +11,9 @@ class home(View):
 	def get(self, request):
 		return render(request ,'home.html')
 
-def contact(request):
 
+
+def contact(request):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
 	form = ContactForm(request.POST or None)
@@ -33,5 +34,86 @@ def contact(request):
 # 		return Contact.objects.all()
 
 def contactList(request):
-	queryset = Contact.objects.all()
+	queryset = Contact.objects.filter(user=request.user)
 	return render(request, 'contact_list.html', {"query": queryset})
+
+# def register(request):    
+# 	form = UserForm(request.POST or None)
+# 	if form.is_valid():
+# 		user = form.save(commit=False)
+# 		username = form.cleaned_data['username']
+# 		password = form.cleaned_data['password']
+# 		user.set_password(password)
+# 		user.save()
+# 		user = authenticate(username=username, password=password)
+# 		if user is not None:
+# 			if user.is_active:
+# 				login(request, user)
+# 				query = Contact.objects.filter(user=request.user)
+# 				return render(request, 'contact_list.html', {"query":query})
+# 	context = {
+# 		"form": form,
+# 	}
+# 	return render(request, 'register.html', form)
+
+class register(View):
+	form_class = UserForm
+	template_name = 'register.html'
+
+	def get(self, request): 
+		form = self.form_class(None)
+		return render(request, self.template_name, {'form': form})
+
+	def post(self, request):
+		form = self.form_class(request.POST)
+
+		if form.is_valid():
+			user = form.save(commit=False)
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			user.set_password(password)
+			user.save()
+			auth_user = authenticate(username=username, password=password)
+			
+			if auth_user is not None:
+
+				if auth_user.is_active:
+					login(request, auth_user)
+					return  redirect('manager:home')
+
+		return render(request, self.template_name, {'form': form})
+
+def logout(request):
+	if request.user.is_authenticated():
+		logout(request)
+		return redirect('manager:home')
+	else:	
+		return render(request, 'not_logged_in.html')
+
+
+
+
+class login(View):
+	form = LoginForm
+	template_name = 'login.html'
+
+	def get(self, request): 
+		form = self.form(None)
+		return render(request, self.template_name, {'form': form})
+
+	def post(self, request):
+		form = self.form(request.POST)
+
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+
+			auth_user = authenticate(username=username, password=password)
+			if auth_user is not None:
+				if auth_user.is_active:
+					login(request, auth_user)
+					return  render(request, 'home.html')
+
+			else:
+				template_name = 'invalid_login.html'
+				return render(request, template_name)
